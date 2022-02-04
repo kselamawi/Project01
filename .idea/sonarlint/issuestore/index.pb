a566ca6ea8ef512c5230f94c48e8bae68ef2e772
isaiah9509 <payne15isaiah@gmail.com>
2022-02-04 17:23:54 -0500

    Created userDao CRUD

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -3,7 +3,7 @@ n
 >app/src/main/java/com/revature/models/ReimbursementStatus.java,c\3\c30fddb0439f58ca7b412c2eda9ea7d84858f8d8
 l
 <app/src/main/java/com/revature/models/ReimbursementType.java,9\0\909e8eed50690cb1723178bf35564ef65db291b9
-_
-/app/src/main/java/com/revature/models/User.java,f\f\fff6efe38be8224006013f88454fc3e0dd198250
-c
-3app/src/main/java/com/revature/models/UserRole.java,8\1\8163c4788922234e5e1d515293096577225c4471
+d
+4app/src/main/java/com/revature/util/LoggingUtil.java,4\d\4dc2d9d52b482177579b3723417e4e2ac3f76ccb
+?
+settings.gradle,0\5\05efc8b1657769a27696d478ded1e95f38737233
--- app/src/main/java/com/revature/ReimbursementDriver.java
+++ app/src/main/java/com/revature/ReimbursementDriver.java
@@ -4,7 +4,12 @@ import com.revature.dao.ReimbursementDao;
 import com.revature.dao.ReimbursementDaoImpl;
 import com.revature.dao.UserDao;
 import com.revature.dao.UserDaoImpl;
+import com.revature.models.User;
+import com.revature.models.UserRole;
 import io.javalin.Javalin;
+
+import java.sql.Connection;
+import java.util.List;
 
 public class ReimbursementDriver {
 
@@ -16,8 +21,19 @@ public class ReimbursementDriver {
         ReimbursementDao reimbursementDao = new ReimbursementDaoImpl();
         UserDao userDao = new UserDaoImpl();
 
-        
+        //Testing creating a user.
+ //       User testUser = new User("Jeremiah", "Grimes", UserRole.MANAGER, "jeremiah.grimes@email.com", "password");
+   //     userDao.createUser(testUser);
 
+        //Testing deleting a user.
+//        userDao.deleteUser(2);
+
+        //Testing Updating user.
+        List<User> userList = userDao.getAllUsers();
+        System.out.println(userList);
+        User testUser = new User("Selamawi", "Kahsai", UserRole.MANAGER, "selamawi.kahsai@email.com", "password");
+        testUser.setId(3);
+        userDao.updateUser(testUser);
 
     }
 
