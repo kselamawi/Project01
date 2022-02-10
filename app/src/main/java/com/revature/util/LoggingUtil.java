00845c8c1453cb449dac1a7719a6386359c0bb4d
Selamawi Kahsai <kselamawi@gmail.com>
2022-02-10 17:49:30 -0500

    "Controller/logging"

--- app/src/main/java/com/revature/util/LoggingUtil.java
+++ app/src/main/java/com/revature/util/LoggingUtil.java
@@ -1,28 +1,9 @@
 package com.revature.util;
-
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
+import org.apache.log4j.Logger;
 import java.util.Date;
 
 public class LoggingUtil {
     private Date date = new Date();
-    private static Logger logger = LoggerFactory.getLogger(LoggingUtil.class);
-
-    public void logConnection() { logger.info("Database connection at " + date);}
-
-    public LoggingUtil() {
-    }
-
-    public void logSuccessfulQuery(String query){logger.info(query + "was successful");}
-
-    public void logger(String info){
-        logger.info(info);
-    }
-
-    public void queryLogger(String query){
-        logger.info(query + " was just attempted");
-    }
+    public final static Logger logger = Logger.getLogger(LoggingUtil.class);
 
 }
