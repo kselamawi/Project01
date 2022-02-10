4789fbe86a92a81ae3c52526f0514e1c7aa598ba
germygrimes <germygrimes@gmail.com>
2022-02-10 18:55:42 -0500

    "Added log4j.properties file and added Configurator to main to get logging to work"

--- app/src/main/java/com/revature/ReimbursementDriver.java
+++ app/src/main/java/com/revature/ReimbursementDriver.java
@@ -1,7 +1,10 @@
 package com.revature;
 
+import org.apache.log4j.BasicConfigurator;
+
 public class ReimbursementDriver {
     public static void main(String[] args) {
+        BasicConfigurator.configure();
         JavalinApp app = new JavalinApp();
         app.start(8070);
     /*    try {
--- app/src/main/java/com/revature/util/LoggingUtil.java
+++ app/src/main/java/com/revature/util/LoggingUtil.java
@@ -1,9 +1,16 @@
 package com.revature.util;
+// Import log4j classes.
+
 import org.apache.log4j.Logger;
+
 import java.util.Date;
 
 public class LoggingUtil {
+
     private Date date = new Date();
-    public final static Logger logger = Logger.getLogger(LoggingUtil.class);
+
+    public static Logger logger = Logger.getLogger(LoggingUtil.class);
+    Logger root = Logger.getRootLogger();
+
 
 }
