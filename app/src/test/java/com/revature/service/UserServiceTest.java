5a291e2a942bcaa68211a1c530e0406e922a4eb5
germygrimes <germygrimes@gmail.com>
2022-02-13 12:10:34 -0500

    "blank"

--- app/src/test/java/com/revature/service/UserServiceTest.java
+++ app/src/test/java/com/revature/service/UserServiceTest.java
@@ -5,8 +5,10 @@ import com.revature.dao.UserDao;
 import com.revature.models.Reimbursement;
 import com.revature.models.User;
 import com.revature.models.UserRole;
+import com.revature.util.LoggingUtil;
+import com.sun.media.jfxmedia.logging.Logger;
 import org.junit.Before;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
@@ -33,16 +35,22 @@ class UserServiceTest {
 
     @Test
     void testCreateUser(){
-        user = new User();
+        user = new User(89, "Paul", "Ball", UserRole.EMPLOYEE, "paul.ball@gmail.com", "password");
         when(userDao.createUser(user)).thenReturn(true);
         assertTrue(userService.createUser(user));
     }
 
     @Test
     void testIfUserExistsById() {
-        user = new User(89, "Paul", "Ball", UserRole.EMPLOYEE, "paul.ball@gmail.com", "password");
-        when(userDao.getUserById(89)).thenReturn(user);
+        user = new User(94, "Will", "Mill", UserRole.EMPLOYEE, "will.mill@gmail.com", "password");
+        when(userDao.getUserById(94)).thenReturn(user);
         assertTrue(userService.createUser(user));
+        LoggingUtil.logger.info(user);
+    }
+
+    @Test
+    void itShouldCheckIfUserExistById() {
+
     }
 
     @Test
