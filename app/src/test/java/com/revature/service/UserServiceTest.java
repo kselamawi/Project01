096b515b45f7c32b0848e4fad9007a766b64db2d
germygrimes <germygrimes@gmail.com>
2022-02-13 10:30:44 -0500

    "incorporating Isaiah's work fixing tests"

--- app/src/test/java/com/revature/service/UserServiceTest.java
+++ app/src/test/java/com/revature/service/UserServiceTest.java
@@ -1,31 +1,11 @@
 package com.revature.service;
 
-import com.revature.dao.ReimbursementDao;
-import com.revature.dao.UserDao;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mock;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.mockito.Mockito.when;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
-
-@ExtendWith(MockitoExtension.class)
 class UserServiceTest {
 
-    @Mock
-    UserDao ud;
-    ReimbursementDao rd;
-
-
     @Test
-    void testIfUserExistById() {
-        assertNotNull(ud);
-        when(ud.getUserById(1)).thenReturn(true);
-        
+    void itShouldCheckIfUserExistById() {
 
     }
 
