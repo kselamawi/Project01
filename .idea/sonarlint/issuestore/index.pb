e1a5e27d73b90c09b4e0c5727722651425bc42f8
germygrimes <germygrimes@gmail.com>
2022-02-13 11:12:28 -0500

    "moving stuff around trying to get things working"

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -3,3 +3,5 @@ _
 /app/src/main/java/com/revature/dao/UserDao.java,2\c\2c56241303a0b05da34118718b96f439ea0cb100
 ?
 settings.gradle,0\5\05efc8b1657769a27696d478ded1e95f38737233
+H
+app/src/log4j.properties,7\8\785ee870728c6279c23fd761ed6a7b7f5a1490b7
--- app/src/test/java/com/revature/service/UserServiceTest.java
+++ app/src/test/java/com/revature/service/UserServiceTest.java
@@ -1,12 +1,48 @@
 package com.revature.service;
 
-import org.junit.jupiter.api.Test;
+import com.revature.dao.ReimbursementDao;
+import com.revature.dao.UserDao;
+import com.revature.models.Reimbursement;
+import com.revature.models.User;
+import com.revature.models.UserRole;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.when;
 
 class UserServiceTest {
 
-    @Test
-    void itShouldCheckIfUserExistById() {
+    @Mock
+    UserDao userDao;
+    ReimbursementDao reimbursementDao;
 
+    @InjectMocks
+    UserService userService;
+    ReimbursementService reimbursementService;
+    User user;
+    Reimbursement reimbursement;
+
+    @Before
+    public void setup(){
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    void testCreateUser(){
+        user = new User();
+        when(userDao.createUser(user)).thenReturn(true);
+        assertTrue(userService.createUser(user));
+    }
+
+    @Test
+    void testIfUserExistsById() {
+        user = new User(89, "Paul", "Ball", UserRole.EMPLOYEE, "paul.ball@gmail.com", "password");
+        when(userDao.getUserById(89)).thenReturn(user);
+        assertTrue(userService.createUser(user));
     }
 
     @Test
