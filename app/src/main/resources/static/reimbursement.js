a5fd2fb38f88c1454ab8c88a26b913d43d11170c
isaiah9509 <payne15isaiah@gmail.com>
2022-02-17 18:00:59 -0500

    Adding HTMLgit add .

--- app/src/main/resources/static/reimbursement.js
+++ app/src/main/resources/static/reimbursement.js
@@ -1,13 +1,58 @@
 
- var allpd = document.getElementById('view-all button');
+ var allpd = document.getElementById('view-pending-button');
+ var allrd = document.getElementById('view-resolved-button');
+ var approveReimb = document.getElementById("approve-button");
+ var selectObjects = document.getElementsByClassName("not-selected");
+
+
+ approveReimb.addEventListener('click', () => {
+    const URL ='http://localhost:7070/';
+
+   var apiUrl = `http://localhost:7070/reimbursements/${document.getElementById("reimbID").value}/approve`;
+   console.log(apiUrl);
+    // document.getElementById("cookiesomething")
+    var authorID = 3;
+    
+    fetch (apiUrl, {
+        method:"PUT",
+        headers : {
+        'Authorization':'MANAGER',
+    }, 
+        body: JSON.stringify({"id": 3}),
+        })
+    })
+
+ allrd.addEventListener('click', () => {
+    const URL ='http://localhost:7070/';
+    var newElement = document.createElement("ul");
+    var pendingList = document.getElementsByClassName("resolved-list");
+    
+    fetch (apiUrl = URL + "reimbursements/resolved",{
+        method:"GET",
+        headers : {
+        'Authorization':'MANAGER',
+        'Content-Type':'application/json'
+    }
+        })
+    
+    .then ((res) => res.json())
+    .then((data) => {console.log(data)
+        console.log(data[1]);
+        for(var i = 0; i < data.length; i++){
+            console.log(`<li>ID:${data[i].id}</li>`);
+            newElement.innerHTML += (`<li class="not-selected">ID:${data[i].id} Amount: ${data[i].amount} Type: ${data[i].reimbursementType} Status: ${data[i].reimbursementStatus}</li>`);
+        }
+        document.getElementById("ResolvedReimbursements").appendChild(newElement);
+    })
+    
+    })
+
  allpd.addEventListener('click', () => {
-const URL ='http://http://34.138.63.74/:7070/reimbursements';
-
+const URL ='http://localhost:7070/';
 var newElement = document.createElement("ul");
-
 var pendingList = document.getElementsByClassName("pending-list");
 
-fetch (apiURL,{
+fetch (apiUrl = URL + "reimbursements/pending",{
     method:"GET",
     headers : {
     'Authorization':'MANAGER',
@@ -20,9 +65,11 @@ fetch (apiURL,{
     console.log(data[1]);
     for(var i = 0; i < data.length; i++){
         console.log(`<li>ID:${data[i].id}</li>`);
-        newElement.innerHTML += (`<li>ID:${data[i].id} Amount: ${data[i].amount} Type: ${data[i].reimbursementType} Status: ${data[i].reimbursementStatus}</li>`);
+        newElement.innerHTML += (`<li class="not-selected">ID:${data[i].id} Amount: ${data[i].amount} Type: ${data[i].reimbursementType} Status: ${data[i].reimbursementStatus}</li>`);
     }
     document.getElementById("PendingReimbursements").appendChild(newElement);
 })
+
+
 
 })
